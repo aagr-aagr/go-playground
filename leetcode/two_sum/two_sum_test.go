10209739981b20976c0035b50e415deb11a13dc5
aagr <job.aagr@gmail.com>
2026-07-27 13:02:00 +0900

    Adds two_sum solution using a map

--- leetcode/two_sum/two_sum_test.go
+++ leetcode/two_sum/two_sum_test.go
@@ -13,6 +13,7 @@ func TestTwoSum(t *testing.T) {
 		want   []int
 	}{
 		{"basic_pair", []int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{"middle_index", []int{3, 2, 4}, 6, []int{1, 2}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
