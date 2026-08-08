a256c00d51fdcc56e7f4e4faaf0c33bc6a91808b
aagr <job.aagr@gmail.com>
2026-08-08 18:20:19 +0900

    Adds top k problem solution with heap and sort

--- leetcode/top_k_frequent_elements/top_k_frequent_elements.go
+++ leetcode/top_k_frequent_elements/top_k_frequent_elements.go
@@ -1,3 +1,86 @@
 package topkfrequentelements
 
-func topKFrequent(nums []int, k int) []int {}
+import (
+	"container/heap"
+	"slices"
+)
+
+type kv struct {
+	Key   int
+	Value int
+}
+
+type kvHeap []kv
+
+func (h kvHeap) Len() int {
+	return len(h)
+}
+
+func (h kvHeap) Less(i, j int) bool {
+
+	return h[i].Value < h[j].Value
+}
+
+func (h kvHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *kvHeap) Push(x any) {
+	*h = append(*h, x.(kv))
+}
+
+func (h *kvHeap) Pop() any {
+	i := len(*h) - 1
+	v := (*h)[i]
+	*h = (*h)[:i]
+	return v
+}
+
+func topKFrequent(nums []int, k int) []int {
+
+	result := make([]int, k)
+	h := make(kvHeap, 0, len(nums))
+	counts := make(map[int]int)
+	heap.Init(&h)
+
+	for _, v := range nums {
+		counts[v]++
+	}
+
+	for i, j := range counts {
+		heap.Push(&h, kv{Key: i, Value: j})
+		if h.Len() > k {
+			heap.Pop(&h)
+		}
+	}
+
+	for i := 0; i < k; i++ {
+		result[i] = h[i].Key
+	}
+	return result
+
+}
+
+func topKFrequentSort(nums []int, k int) []int {
+
+	result := make([]int, k)
+	counts := make(map[int]int)
+
+	for _, v := range nums {
+		counts[v]++
+	}
+
+	var list []kv
+	for num, v := range counts {
+		list = append(list, kv{num, v})
+	}
+	slices.SortFunc(list, func(a, b kv) int {
+		return b.Value - a.Value
+	})
+
+	for i := 0; i < k; i++ {
+		result[i] = list[i].Key
+	}
+	return result
+
+}
--- leetcode/top_k_frequent_elements/top_k_frequent_elements_test.go
+++ leetcode/top_k_frequent_elements/top_k_frequent_elements_test.go
@@ -1,11 +1,31 @@
 package topkfrequentelements
 
-func TestTopKFrequent(nums []int, k int) []int {
+import (
+	"slices"
+	"testing"
+)
+
+func TestTopKFrequent(t *testing.T) {
 	tests := []struct {
 		name   string
 		input  []int
 		count  int
 		result []int
 	}{
+		{"test1", []int{1, 1, 1, 2, 2, 3}, 2, []int{1, 2}},
+		{"test2", []int{3, 1, 3, 2, 2, 2, 3, 3, 4, 5, 1, 1, 1, 1, 3, 2}, 3, []int{1, 2, 3}},
+		{"test3", []int{1}, 1, []int{1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := topKFrequent(tt.input, tt.count)
+			slices.Sort(got)
+			slices.Sort(tt.result)
+			if !slices.Equal(got, tt.result) {
+				t.Errorf("Slices %v and %v -- not equal", got, tt.result)
+			}
+
+		})
+	}
 
 }
